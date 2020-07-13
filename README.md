4364a3b2874363db39b3404d42e6c34bc310a4f8
Lyne <lyner@elslapm-157108.home>
2020-07-13 15:25:08 +0100

    RL: adding hashmap macro as initial toolbox commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rusty Toolbox
+
+A small library of useful miscellaneous functions to save writing them over and over again.
---
+++ src/collections.rs
@@ -0,0 +1,9 @@
+//// https://stackoverflow.com/a/28392068/3747705
+#[macro_export]
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
